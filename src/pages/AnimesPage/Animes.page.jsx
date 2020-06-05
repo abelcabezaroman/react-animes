4a6d3e30c1f99e7860462c35053f5b2c9b3a0854
Abel Cabeza Román <contacto@abelcabezaroman.com>
2020-06-05 18:26:30 +0200

    paginator

--- src/pages/AnimesPage/Animes.page.jsx
+++ src/pages/AnimesPage/Animes.page.jsx
@@ -2,51 +2,59 @@ import React, { useContext, useEffect, useState } from 'react';
 import axios from "axios";
 import { environment } from "../../environments/environment";
 import { AnimeGallery } from "../../shared/components/AnimeGallery/AnimeGallery";
+import { AnimeSearch } from "../../shared/components/AnimeSearch/AnimeSearch";
+
+const itemsPerPage = 19;
+const defaultPageInfo = { first: 1, rows: itemsPerPage };
 
 export function AnimesPage () {
 
     const [animes, setAnimes] = useState([]);
-
-    // const [amiibos, setAmiibos] = useContext(AmiibosContext);
-    // const [filteredAmiibos, setFilteredAmiibos] = useState([]);
+    const [pageInfo, setPageInfo] = useState(defaultPageInfo);
+    const [filterValues, setFilterValues] = useState({ name: '', category: '' });
+    const [totalRecors, setTotalRecords] = useState(0);
 
 
     useEffect(() => {
-        axios.get(environment.url + 'anime?page[limit]=15').then(res => {
+        axios.get(environment.url + 'anime?page[limit]=' + itemsPerPage + '&page[offset]=0').then(res => {
             const animesLocal = res.data.data;
-            console.log('##ABEL## >> AnimesPage >>  res.data.data', res.data.data);
             setAnimes(animesLocal);
-            // setFilteredAmiibos(amiibosLocal)
+            setTotalRecords(res.data.meta.count / itemsPerPage);
         })
     }, [])
 
-    //
-    // const filterAmiibos = (filterValues) => {
-    //     const filteredLocalAmiibos = [];
-    //     for (const amiibo of amiibos) {
-    //         let amiiboIsOk = true;
-    //         for (const key in filterValues) {
-    //             if (filterValues.hasOwnProperty(key)) {
-    //                 const localValue = amiibo[key];
-    //                 const filterValue = filterValues[key];
-    //                 if (!localValue || (localValue && filterValue && filterValue !== '' && !localValue.toLowerCase().includes(filterValue.toLowerCase()))) {
-    //                     amiiboIsOk = false;
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         if (amiiboIsOk) {
-    //             filteredLocalAmiibos.push(amiibo);
-    //         }
-    //     }
-    //     setFilteredAmiibos(filteredLocalAmiibos);
-    // }
+
+    const filterAnimes = (filterValues) => {
+        setFilterValues(filterValues);
+        setPageInfo(defaultPageInfo);
+        axios.get(environment.url + 'anime?page[limit]=' + itemsPerPage + '&page[offset]=0' + createFilterUrl(filterValues)).then(res => {
+            const animesLocal = res.data.data;
+            setAnimes(animesLocal);
+        })
+    }
+
+    const changePage = (pageInfo) => {
+        console.log('##ABEL## >> AnimesPage >>  changePage', pageInfo);
+        setPageInfo(pageInfo);
+
+        axios.get(environment.url + 'anime?page[limit]=' + itemsPerPage + '&page[offset]=' + pageInfo.first + createFilterUrl(filterValues)).then(res => {
+            const animesLocal = res.data.data;
+            setAnimes(animesLocal);
+        })
+    }
+
+    const createFilterUrl = (filterValues) => {
+        let filterUrl = filterValues.name.length ? '&filter[text]=' + filterValues.name : '';
+        filterUrl += filterValues.category.length ? '&filter[categories]=' + filterValues.category : ''
+        return filterUrl;
+    }
+
 
     return (
         <div>
             <h1 className="b-text-primary d-flex justify-content-center">Animes</h1>
-            {/*<AmiiboSearch fnSubmit={filterAmiibos}/>*/}
-            <AnimeGallery animes={animes}/>
+            <AnimeSearch fnSubmit={filterAnimes}/>
+            <AnimeGallery totalRecors={totalRecors} pageInfo={pageInfo} animes={animes} fnChangePage={changePage}/>
         </div>
     );
 }
--- src/shared/components/AnimeGallery/AnimeGallery.jsx
+++ src/shared/components/AnimeGallery/AnimeGallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AnimeGallery.scss'
-import { Link } from "react-router-dom";
+import {Paginator} from 'primereact/paginator';
 
 
 export function AnimeGallery (props) {
@@ -17,22 +17,27 @@ export function AnimeGallery (props) {
     //     background-color: ${({ theme }) => theme.backgroundOpacity};
     // }`;
 
+
+
     return (
         <div className="c-anime-gallery">
             <div className="row">
                 {props.animes.map((item, index) =>
                     <div className={index < 3 ? "col-4" : "col-3"} key={index}>
-                        <Link to={'animes/' + item.attributes.id} className="c-anime-gallery__item-container">
+                        <a href={'https://kitsu.io/anime/' + item.id}  target="_blank" className="c-anime-gallery__item-container">
                             <figure className={index < 3 ? "c-anime-gallery__item c-anime-gallery__item--max" : "c-anime-gallery__item"}>
                                 <img src={item.attributes.posterImage && item.attributes.posterImage.original} alt=""
                                      className="c-anime-gallery__img"/>
                                 <figcaption
                                     className="c-anime-gallery__caption">{item.attributes.canonicalTitle}</figcaption>
                             </figure>
-                        </Link>
+                        </a>
                     </div>
                 )}
             </div>
+
+            <Paginator className="b-primereact-paginator" first={props.pageInfo.first} rows={props.pageInfo.rows} totalRecords={props.totalRecors} onPageChange={props.fnChangePage}/>
+
         </div>
 
 
